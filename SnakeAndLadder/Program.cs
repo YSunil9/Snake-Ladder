4ea028fbdd5f9d39812513a3008b6173abc45b69
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-14 16:29:09 +0530

    UC4 Code

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,33 +4,57 @@
     {
         static void Main(string[] args)
         {
-        
-            int winPos   = 100;
-            int diceVal  = 0;
+            int playerpos = 0;
+            int winPos = 100;
+            int diceVal = 0;
             int startPos = 0;
-            int currPos  = 0;   
-            Random random = new Random();
-            int dice = random.Next(1,7);
-            Console.WriteLine("Dice Num : " + dice);
-            Random randomNum = new Random();
-            int option=randomNum.Next(3);   
-            switch(option)
-            {
-                case 1: 
-              currPos += dice;
-                    Console.WriteLine("Ladder");
-                    break;
+            int currPos = 0;
+           
+          
+            
 
-                case 2:
-                    currPos -= dice;
-                    Console.WriteLine("Snake");
-                    break;
-                default:
-                    Console.WriteLine("No Play");
-                    break;
-            }
-        }
+            while (  currPos<100 )
+            {
+                Random randomNum = new Random();
+                int option = randomNum.Next(3);
+                Random random = new Random();
+                int dice = random.Next(1, 7);
+                Console.WriteLine("Dice Num : " + dice);
+
+                switch (option)
+                {
+
+                    case 1:
+                        currPos += dice;
+                        Console.WriteLine("Ladder");
+                        Console.WriteLine("CurrPos" + currPos);
+                        if(currPos > 100)
+                        {
+                            currPos -=dice;
+                        }
+
+                        break;
+
+                    case 2:
+                        currPos -= dice;
+                        Console.WriteLine("Snake");
+                        Console.WriteLine("CurrPos" + currPos);
+                        if (currPos < 0)
+                        {
+                            currPos=0;
+                        }
+                        
+                        break;
+                    default:
+                        Console.WriteLine("No Play");
+                        Console.WriteLine("CurrPos" + currPos);
+                        break;
+                }
                
+
+            }
+            Console.WriteLine("CurrPos" + currPos);
+        }
+
     }
 }
- 
