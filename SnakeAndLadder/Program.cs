d2a9ac665105288ca18f605e53c0ba27886c7623
Ysunil9 <sunil.pcs24@gmail.com>
2022-11-14 09:10:54 +0530

    UC3 Code

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -4,16 +4,33 @@
     {
         static void Main(string[] args)
         {
-            int pos = 0;
-            Console.WriteLine("single player at start position", +pos);
-            RollDice();
-        }
-         public static void  RollDice()
-        {
+        
+            int winPos   = 100;
+            int diceVal  = 0;
+            int startPos = 0;
+            int currPos  = 0;   
             Random random = new Random();
-            int dice = random.Next(6);
-            dice = dice + 1;
+            int dice = random.Next(1,7);
             Console.WriteLine("Dice Num : " + dice);
+            Random randomNum = new Random();
+            int option=randomNum.Next(3);   
+            switch(option)
+            {
+                case 1: 
+              currPos += dice;
+                    Console.WriteLine("Ladder");
+                    break;
+
+                case 2:
+                    currPos -= dice;
+                    Console.WriteLine("Snake");
+                    break;
+                default:
+                    Console.WriteLine("No Play");
+                    break;
+            }
         }
+               
     }
 }
+ 
